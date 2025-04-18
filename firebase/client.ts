b66030ea45360ee19ea9ee0ea0f4b93d58c822d6
Charith Wickramasinghe <pvmcharith@gmail.com>
2025-04-18 18:44:51 +0530

    setup api routes

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAWtevGdpmV_FxwGrPl7K_MSWxpHjdTIlw',
-  authDomain: 'prepwise-44f15.firebaseapp.com',
-  projectId: 'prepwise-44f15',
-  storageBucket: 'prepwise-44f15.firebasestorage.app',
-  messagingSenderId: '497639213113',
-  appId: '1:497639213113:web:54233c63a6886a7baaa5e8',
-  measurementId: 'G-5KLSLB77ZJ',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
